650f73bcd83626aba7e26d23111f65901c4ab646
Cauã Parreiras Vieira <89464308+Kuriei@users.noreply.github.com>
2026-09-03 12:00:45 -0300

    Create rover.js

---
+++ rover.js
@@ -0,0 +1,9 @@
+// rover.js
+
+function inicializarRover() {
+    console.log("Sistemas do Rover iniciados!");
+    console.log("Painéis solares: OK");
+    console.log("Nível de bateria: 100%");
+}
+
+inicializarRover();
